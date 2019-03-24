10ba3be0a91341db9b365558cbd8cd2d1490b6c9
rajeswari <rajeswari@calibraint.com>
2019-03-24 11:39:14 +0530

    steps with recursion

--- src/app/training/steps-problem/steps.component.ts
+++ src/app/training/steps-problem/steps.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import * as _ from 'lodash';
-import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
     selector: 'app-steps',
@@ -33,31 +32,6 @@ export class StepsComponent {
         console.log(this.posibilities);
     }
 
-    // calculateArray() {
-    //     this.result = _.fill(Array(this.steps + 1), 1);
-    //     console.log(this.result);
-    //     this.skipArray = _.slice(_.times(this.highestSkip + 1, Number), this.leastSkip, this.highestSkip + 1);
-    //     console.log(this.skipArray);
-    //     _.forEach(this.result, (possibilities, step) => {
-    //         if (step >= this.leastSkip) {
-    //             _.forEach(this.skipArray, (skipCount, index) => {
-    //                 if (_.includes(this.skipArray, step)) {
-    //                     if (_.includes(this.skipArray, (step - skipCount)) || (step === skipCount)) {
-    //                         this.result[step] += this.result[step - skipCount];
-    //                     }
-    //                     console.log(_.includes(this.skipArray, step));
-    //                     this.result[step] = step > skipCount ? (this.result[skipCount] + this.result[step]) : 1;
-    //                 } else {
-    //                     this.result[step] += this.result[step - skipCount];
-    //                 }
-    //             }
-    //             );
-    //         } else {
-    //             this.result[step] = 0;
-    //         }
-    //     });
-    //     console.log(this.result);
-    // }
     calculateArray() {
         this.result = _.fill(Array(this.steps + 1), 0);
         this.skipArray = _.slice(_.times(this.highestSkip + 1, Number), this.leastSkip, this.highestSkip + 1);
@@ -75,5 +49,31 @@ export class StepsComponent {
         this.posibilities = _.reduce(this.result, (add, posibility) =>
             add + posibility);
         console.log(this.result, this.posibilities);
+    }
+
+    calculateRecursive() {
+        this.result = _.fill(Array(this.steps + 1), 0);
+        this.result[this.leastSkip] = 1;
+        this.result[this.highestSkip] = 1;
+        console.log(this.result);
+        this.posibilities = this.possibility(this.leastSkip);
+    }
+
+    possibility(step) {
+        if ((step - this.leastSkip) > 0) {
+            this.result[step] += this.result[step - this.leastSkip];
+            if ((step - this.highestSkip) > 0) {
+                this.result[step] += this.result[step - this.highestSkip];
+            }
+            if (step < this.steps) {
+                console.log(this.result);
+                return this.possibility(step + 1);
+            } else {
+                return _.reduce(this.result, (add, posibility) =>
+                    add + posibility);
+            }
+        } else {
+            return this.possibility(step + 1);
+        }
     }
 }
